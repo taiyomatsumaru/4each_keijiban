2e2cc1e7c0090b4aeb2ed496be138a441d32215c
taiyomatsumaru <84229227+TaiyoMatsumaru@users.noreply.github.com>
2022-05-26 22:06:10 +0900

    PHPで作成した4eachの掲示板

---
+++ index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>4eachblog 掲示板</title>
+    <link rel="stylesheet"type="text/css" href="style.css">
+</head>
+
+<body>
+
+    <?php
+
+    mb_internal_encoding("utf8");
+    $pdo = new PDO("mysql:dbname=lesson01;host=localhost;","root","");
+    $stmt = $pdo->query("select * from 4each_keijiban");
+    
+    ?>
+
+    <div class="logo">
+        <img src="4eachblog_logo.jpg">
+    </div>
+    <header>
+        <ul>
+            <li>トップ</li>
+            <li>プロフィール</li>
+            <li>4eachについて</li>
+            <li>登録フォーム</li>
+            <li>問い合わせ</li>
+            <li>その他</li>
+        </ul>
+    </header>
+    <main>
+   
+        <div class="left">
+            <h1>プログラミングに役立つ掲示板</h1>
+            <div class="form">
+                <h2>入力フォーム</h2>
+                <form method="post" action="insert.php">
+                    <div>
+                        <label>ハンドルネーム</label>
+                        <br>
+                        <input type="text" class="text" size="65" name="handlename">
+                    </div>
+
+                    <div>
+                        <label>タイトル</label>
+                        <br>
+                        <input type="text" class="text" size="65" name="title">
+                    </div>
+
+                    <div>
+                        <label>コメント</label>
+                        <br>
+                        <textarea cols="95" rows="10" name="comments"></textarea>
+                    </div>  
+                    <input type="submit" class="submit" value="投稿する">
+                </form>
+            </div>
+
+            <?php
+
+            while($row=$stmt->fetch()){
+
+            echo "<div class='kiji'>";
+            echo "<h3>".$row['title']."</h3>";
+            echo "<div class='contents'>";
+            echo "<h5>".$row['comments']."<h5>";
+            echo "<div class='handlename'>posted by".$row['handlename']."</div>";
+            echo "</div>";
+            echo "</div>";
+            }
+            
+            ?>
+            
+            <?php
+
+            while($row=$stmt->fetch()){
+
+            echo "<div class='kiji'>";
+            echo "<h3>".$row['title']."</h3>";
+            echo "<div class='contents'>";
+            echo "<h5>".$row['comments']."<h5>";
+            echo "<div class='handlename'>posted by".$row['handlename']."</div>";
+            echo "</div>";
+            echo "</div>";
+            }
+
+            ?>
+        </div>     
+        <div class="right">
+            <h2>人気の記事</h2>
+            <ul>
+                <li>PHPオススメ本</li>
+                <li>PHP MyAdminの使い方</li>
+                <li>今人気のエディタ Top5</li>
+                <li>HTMLの基礎</li>
+            </ul>
+            <h2>オススメリンク</h2>
+            <ul>
+                <li>インターノウス株式会社</li>
+                <li>XAMPPのダウンロード</li>
+                <li>Eclipseのダウンロード</li>
+                <li>Bracketsのダウンロード</li>
+            </ul>
+            <h2>カテゴリ</h2>
+            <ul>
+                <li>HTML</li>
+                <li>PHP</li>
+                <li>MySQL</li>
+                <li>JavaScript</li>
+            </ul>
+        </div>
+    </main>
+    <footer>
+        copyright © internous | 4each blog the which provides A to Z about programming.
+    </footer>
+</body>
+</html>
